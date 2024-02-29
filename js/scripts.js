d3140b7c72809aee375f26c8025ca1f4a63c69e0
onorris <160195527+onorris@users.noreply.github.com>
2024-02-29 17:56:47 -0600

    pokemon list created
    Bulbasaur, Charmander, Dratini added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-
+let pokemonList = [
+    {name: 'Bulbasaur',
+    height: 7,
+    types: [
+        'grass',
+        'poison'
+    ]},
+    {name: 'Charmander',
+    height: 6,
+    types: [
+        'fire'
+    ]},
+    {name: 'Dratini',
+    height: 18,
+    types: [
+        'dragon'
+    ]}
+]
