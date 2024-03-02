8ff6f20175bc747145914c4f54665e716a029a43
onorris <160195527+onorris@users.noreply.github.com>
2024-03-01 20:38:49 -0600

    for loop listing pokemon name and height

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,13 @@ let pokemonList = [
         'dragon'
     ]}
 ]
+//For loop listing Pokemon name and height, adds comment if greater than 1
 for (let i = 0; i <pokemonList.length; i++) {
-    document.write('<p>'+pokemonList[i].name + ' (height: ' + pokemonList[i].height+')'+'<p/>')
+    if (pokemonList[i].height > 1) {
+        result = ' - Wow, that\'s big!'
+    } else {
+        result = ''
+    }
+   document.write('<p>'+pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')'+ result +'<p/>')    
+ 
 }
