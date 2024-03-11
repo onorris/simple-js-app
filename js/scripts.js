fb75074348d5b85192fd66701ca7127769ccc1c3
onorris <160195527+onorris@users.noreply.github.com>
2024-03-11 09:46:56 -0500

    Added fonts and removed loop function

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let pokemonList = [
     height: 0.7,
     types: [
         'grass',
-        'poison'
+        'poison',
     ]},
     {name: 'Charmander',
     height: 0.6,
@@ -16,13 +16,3 @@ let pokemonList = [
         'dragon'
     ]}
 ]
-//For loop listing Pokemon name and height, adds comment if greater than 1
-for (let i = 0; i <pokemonList.length; i++) {
-    if (pokemonList[i].height > 1) {
-        result = ' - Wow, that\'s big!'
-    } else {
-        result = ''
-    }
-   document.write('<p>'+pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')'+ result +'<p/>')    
- 
-}
