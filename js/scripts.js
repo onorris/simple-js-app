4f8f6a1c37984e01c6211786d91c21f313d31d3a
onorris <160195527+onorris@users.noreply.github.com>
2024-03-11 13:12:35 -0500

    added filter function

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,22 @@ let pokemonRepository = (function () {
     ]}]
 
     function add(pokemon){
-            pokemonList.push(pokemon);
+        if (typeof pokemon !== 'object' ){//|| typeof pokemon.name !== 'string' || typeof pokemon.height !== 'number' || typeof pokemon.types !== 'object' ){
+            return alert("Cannot add to list")
         }
+        else {pokemonList.push(pokemon);
+        }
+    }
     function getAll(){
             return pokemonList;
         }
+
+    // filter array items based on search criteria (query
+    function filterPokemon(pokemon) {
+        return pokemonList.filter((el) => el.name === pokemon)
+    }
     return {
+        filterPokemon: filterPokemon,
         add: add,
         getAll: getAll
     };
@@ -36,3 +46,8 @@ pokemonRepository.add({name: 'Pikachu', height: 0.4, types:'electric'});
 pokemonRepository.getAll().forEach(function(pokemon) {
     document.write(pokemon.name + ' is a ' + pokemon.types + ' pokemon type and is this tall: ' + pokemon.height + 'm.  ');
 });
+
+
+
+filtered = pokemonRepository.filterPokemon('Dratini')
+console.log(filtered)
